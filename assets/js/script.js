0e4de4a70f1ca42fcb24edd521293459c2dfa515
axeliono <69375446+axeliono@users.noreply.github.com>
2020-11-06 14:28:32 -0600

    pushing changes to figure out spacing issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,18 +12,41 @@ function getRecipe() {
     })
     .then(function(data) {
         let recipesFound = data.hits
+        var cardHolder = document.querySelector(".recipe-card-holder");
+        cardHolder.innerHTML = "";
+
         for (i = 0; i < recipesFound.length; i++) {
-        let recipeName = data.hits[i].recipe.label
-        console.log(recipeName)
-        let recipeImage = data.hits[i].recipe.image
-        console.log(recipeImage)
-        let ingredientList = data.hits[i].recipe.ingredients
-        console.log(ingredientList)
+            let recipeName = data.hits[i].recipe.label;
+            console.log(recipeName);
+            let recipeImage = data.hits[i].recipe.image;
+            console.log(recipeImage);
+            let ingredientList = data.hits[i].recipe.ingredients;
+            console.log(ingredientList);
+
+            displayRecipeCards(recipeName, recipeImage, ingredientList);
+
+        
         }
+
     })
 }
 
+function displayRecipeCards(recipeName, recipeImage, ingredientList) {
+    var cardHolder = document.querySelector(".recipe-card-holder");
 
+    var recipeCard = document.createElement("div");
+    recipeCard.className = "recipe-card";
+    var image = document.createElement("img");
+    image.src = recipeImage;
+
+    recipeCard.innerText = recipeName;
+    recipeCard.appendChild(image);
+
+    cardHolder.appendChild(recipeCard);
+    
+
+
+}
 
 analyzeRecipeEl.addEventListener("click", function() {
     console.log("button pressed");
