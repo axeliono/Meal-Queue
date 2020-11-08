ed063123d65d37281e6fc9d46d490990c5bd54cc
Joshua Sung <yuniksung@gmail.com>
2020-11-07 19:19:10 -0600

    created search-history box next to analyze button

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,10 +44,13 @@ function displayRecipeCards(recipeName, recipeImage, ingredientList) {
     var imgContainer = document.createElement("div");
     imgContainer.setAttribute("class", "img-container");
     var image = document.createElement("img");
+
     
+
     recipeCard.className = "recipe-card";
     image.src = recipeImage;
 
+    
 
     recipeCardTxt.innerText = recipeName;
     recipeCard.appendChild(txtContainer)
@@ -56,9 +59,49 @@ function displayRecipeCards(recipeName, recipeImage, ingredientList) {
     imgContainer.appendChild(image);
     cardHolder.appendChild(recipeCard);
     
+    
+// Joshua's code for modals = you can delete this or keep this in a reference
+//     // Function for modals
+//     let modalEl = () => {
+//     // create div element to contain button
+//     let modalContainer = document.createElement("div");
+//     modalContainer.setAttribute("class", "modal-container");
+//     // Create an input
+//     let modalToggle = document.createElement("input");
+//     modalToggl.setAttribute("id", "modal-toggle")
+//     modalToggle.setAttribute("type", "checkbox");
+//     // Create label for button
+//     let modalBtn = document.createElement("label");
+//     modalBtn.setAttribute("class", "modal-btn");
+//     modalBtn.setAttribute("id", "modal-btn");
+//     modalBtn.setAttribute("for", "modal-toggle");
+//     modalBtn.textContent = "See More";
+//     // Create another label
+//     let modalBackDrop = document.createElement("label");
+//     modalBackDrop.setAttribute("class", "modal-backdrop");
+//     modalBackDrop.setAttribute("for", "modal-toggle");
 
+//     // create div element for popup window
+//     let modalContent = document.createElement("div");
+//     modalContent.setAttribute("class", "modal-content");
 
-}
+//     // Create label for "X" icon
+//     let modalClose = document.createElement("label");
+//     modalClose.setAttribute("class", "modal-close");
+//     modalClose.setAttribute("for", "modal-toggle");
+
+//     modalClose.innerText = "&#x2715;";
+
+//     // Create an eventlistner for modalBtn
+//     modalBtn.addEventListener("click", function(){
+//         // For Jullia and Peter to write a function - if user clicks modalBtn, it directs to the content
+//     });
+
+//     modalContainer.appendChild(modalBtn);
+//     recipeCard.appendChild(modalContainer);
+//     }
+
+// }
 
 analyzeRecipeEl.addEventListener("click", function() {
     console.log("button pressed");
