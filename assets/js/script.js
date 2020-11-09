5f256bbc83e482337316cb63ecfb706dc0e1f3e0
unknown <peters0470@gmail.com>
2020-11-09 14:49:22 -0600

    fix the javascript file after ridiculous mistake

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const appID = "3e035de5";
 const apiKey = "736b0810150196f28b8c1028864f5f3f";
 var analyzeRecipeEl = document.getElementById("result-btn");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+const recipeName = document.getElementById("name-input").value;
+const ytApiKey = "AIzaSyBfA_iWGNboQ7NaUCYZK0b7BytWLfSkbX4";
 //var clearHistoryEl = 
 //var historyEl = document.querySelector(".operation-right");
 //let searchTerm = "";
@@ -25,43 +27,111 @@ function getRecipe(searchTerm) {
             //console.log(recipeImage);
             let ingredientList = data.hits[i].recipe.ingredients;
             //console.log(ingredientList);
-            
-        }
-        
-    
-        
+            var ingredientArrayObject = { ingredients: [] };
+            for (x = 0; x < ingredientList.length; x++) {
+              //put each ingredient into array
+              ingredientArrayObject.ingredients.push(ingredientList[x].text);
+            }
+            console.log(ingredientArrayObject);
             displayRecipeCards(recipeName, recipeImage, ingredientList);
-        
+          }
         
     })
 }
 
 
+function displayRecipeCards(recipeName, recipeImage, ingredientArrayObject) {
+  var cardHolder = document.querySelector(".recipe-card-holder");
 
-function displayRecipeCards(recipeName, recipeImage, ingredientList) {
-    var cardHolder = document.querySelector(".recipe-card-holder");
-    
-    // creates div element to contain recipe card
-    var recipeCard = document.createElement("div");
-    var txtContainer = document.createElement("div");
-    var recipeCardTxt = document.createElement("p");
-    txtContainer.setAttribute("class", "text-container");
+  // creates div element to contain recipe card
+  var recipeCard = document.createElement("div");
+  // take the text of each index of the object holding the array of ingredients
 
-    // creates div element to contain "image"
-    var imgContainer = document.createElement("div");
-    imgContainer.setAttribute("class", "img-container");
-    var image = document.createElement("img");
-    
-    recipeCard.className = "recipe-card";
-    image.src = recipeImage;
+  var txtContainer = document.createElement("div");
+  var recipeCardTxt = document.createElement("p");
+  txtContainer.setAttribute("class", "text-container");
+  recipeCardTxt.innerText = recipeName;
 
-    recipeCardTxt.innerText = recipeName;
-    recipeCard.appendChild(txtContainer)
-    txtContainer.appendChild(recipeCardTxt);
-    recipeCard.appendChild(imgContainer);
-    imgContainer.appendChild(image);
-    cardHolder.appendChild(recipeCard);
+  // creates div element to contain "image"
+  var imgContainer = document.createElement("div");
+  imgContainer.setAttribute("class", "img-container");
+  var image = document.createElement("img");
+
+  recipeCard.className = "recipe-card";
+  image.src = recipeImage;
+
+  //modal button labels and elements
+  var recipeModalButton = document.createElement("div");
+  recipeModalButton.setAttribute("class", "modal-container");
+  var recipeModalButtonInput = document.createElement("input");
+  recipeModalButtonInput.setAttribute("type", "checkbox");
+  recipeModalButtonInput.setAttribute("id", "modal-toggle");
+  var btnLabel = document.createElement("label");
+  btnLabel.setAttribute("for", "modal-toggle");
+  btnLabel.className = "modal-btn";
+  var backdropLabel = document.createElement("label");
+  backdropLabel.setAttribute("for", "modal-toggle");
+  backdropLabel.className = "modal-backdrop";
+  recipeModalButton.appendChild(recipeModalButtonInput);
+  recipeModalButton.appendChild(btnLabel);
+  recipeModalButton.appendChild(backdropLabel);
+
+  //modal content labels and elements
+  recipeModalContentEl = document.createElement("div");
+  recipeModalContentEl.className = "modal-content";
+  var contentLabelEl = document.createElement("label");
+  contentLabelEl.setAttribute("for", "modal-toggle");
+  contentLabelEl.setAttribute("class", "modal-close");
+  contentLabelEl.innerText = "\u2715";
+  var contentText = document.createElement("h2");
+  contentText.innerText = recipeName;
+  recipeModalContentEl.appendChild(contentLabelEl);
+  recipeModalContentEl.appendChild(contentText);
+
+  //set ingredients as attribute to be accessed by modal
+  recipeCard.setAttribute("ingredients", ingredientArrayObject);
+  recipeCard.appendChild(txtContainer);
+  txtContainer.appendChild(recipeCardTxt);
+  recipeCard.appendChild(imgContainer);
+  imgContainer.appendChild(image);
+
+  recipeModalButton.appendChild(recipeModalContentEl);
+
+  recipeCard.appendChild(recipeModalButton);
+  cardHolder.appendChild(recipeCard);
 }
+
+// YOUTUBE API
+function getYT() {
+  var recipeName = document.getElementById("name-input").value;
+  let ytURL = `https://youtube.googleapis.com/youtube/v3/search?&part=snippet&maxResults=2&q=${recipeName} + "making"&key=${ytApiKey}`;
+  fetch(ytURL)
+    .then(function (response) {
+      console.log(recipeName);
+      return response.json();
+    })
+    .then(function (data) {
+      var id = data.items[0].id.videoId;
+      console.log(id);
+      displayYT(id);
+    });
+}
+
+function displayYT(id) {
+  var playerEl = document.getElementById("player");
+  playerEl.setAttribute(
+    "src",
+    "http://www.youtube.com/embed/" +
+      id +
+      "?enablejsapi=1&origin=http://example.com"
+  );
+}
+
+var videoBtnEl = document.getElementById("video-btn");
+videoBtnEl.addEventListener("click", function () {
+  getYT();
+});
+
 // when a recipe is searched its put in local storage
 analyzeRecipeEl.addEventListener("click", function() {
     searchTerm = document.getElementById("name-input").value;
