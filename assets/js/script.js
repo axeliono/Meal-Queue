8be13f04054d0beb90ddc4d1bd7030609866fb0c
j9210 <71297542+j9210@users.noreply.github.com>
2020-11-10 21:32:51 -0600

    YT

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,15 +4,12 @@ const apiKey = "736b0810150196f28b8c1028864f5f3f";
 var analyzeRecipeEl = document.getElementById("result-btn");
 //var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 const recipeName = document.getElementById("name-input").value;
-const ytApiKey = "AIzaSyCwJNSo98gcvzFPQLMWt8HhKVXCMI5weWI";
+const ytApiKey = "AIzaSyBpyJoyYhEEme36o2jXPW1axovfONzciKQ";
 //var clearHistoryEl = 
 //var historyEl = document.querySelector(".operation-right");
 //let searchTerm = "";
 
 function getRecipe(searchTerm) {
-
- 
-    
     let recipeURL = `https://api.edamam.com/search?app_id=${appID}&app_key=${apiKey}&q=${searchTerm}`;
     fetch(recipeURL).then(function(response) {
         return response.json()
@@ -24,18 +21,18 @@ function getRecipe(searchTerm) {
         
         for (i = 0; i < recipesFound.length; i++) {
             let recipeName = data.hits[i].recipe.label;
-            //console.log(recipeName);
+            console.log(recipeName);
             let recipeImage = data.hits[i].recipe.image;
-            //console.log(recipeImage);
+            console.log(recipeImage);
             let ingredientList = data.hits[i].recipe.ingredients;
-            //console.log(ingredientList);
+            console.log(ingredientList);
             var ingredientArrayObject = { ingredients: [] };
             for (x = 0; x < ingredientList.length; x++) {
               //put each ingredient into array
               ingredientArrayObject.ingredients.push(ingredientList[x].text);
             }
             console.log(ingredientArrayObject);
-            displayRecipeCards(recipeName, recipeImage, ingredientList);
+            displayRecipeCards(recipeName, recipeImage, ingredientArrayObject);
           }
         
     })
@@ -53,6 +50,7 @@ function displayRecipeCards(recipeName, recipeImage, ingredientArrayObject) {
   txtContainer.setAttribute("class", "text-container");
   var recipeCardTxt = document.createElement("p");
   recipeCardTxt.innerText = recipeName;
+ 
 
   // creates div element to contain "image"
   var imgContainer = document.createElement("div");
@@ -72,6 +70,7 @@ function displayRecipeCards(recipeName, recipeImage, ingredientArrayObject) {
   btnLabel.setAttribute("for", "modal-toggle");
   btnLabel.className = "modal-btn";
   btnLabel.innerText = "See More"
+  //btnLabel.onclick = getYT();
   var backdropLabel = document.createElement("label");
   backdropLabel.setAttribute("for", "modal-toggle");
   backdropLabel.className = "modal-backdrop";
@@ -104,81 +103,84 @@ function displayRecipeCards(recipeName, recipeImage, ingredientArrayObject) {
   recipeCard.appendChild(recipeModalButton);
   cardHolder.appendChild(recipeCard);
 
-  getYT(recipeName);
+ // getYT(recipeName);
 
 }
 // YOUTUBE API
-function getYT(recipeName) {
-  //var recipeName = document.getElementById("name-input").value;
-  let ytURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=pizza&type=video&key=${ytApiKey}`;
-  fetch(ytURL)
-    .then(function (response) {
-      console.log(recipeName, response);
-      //return response.json();
-    })
-    // .then(function (data) {
-    //   var id = data.items[0].id.videoId;
-    //   console.log(id);
-    //   displayYT(id);
-    // });
-}
+// function getYT(recipeName) {
 
-function displayYT(id) {
-  var playerEl = document.getElementById("player");
-  playerEl.setAttribute(
-    "src",
-    "http://www.youtube.com/embed/" +
-      id +
-      "?enablejsapi=1&origin=http://example.com"
-  );
-}
+//   console.log("WORKING")
+//   //var recipeName = document.getElementById("name-input").value;
+//   let ytURL = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=pizza&type=video&key=${ytApiKey}&maxResults=2`;
+//   // fetch(ytURL)
+//   //   .then(response => response.json())
+//   //   .then( data => {
+//   //     console.log(data.items[0].id.videoId)
+//   //   var id = data.items[0].id.videoId;
+//   //   var videoURL =  "http://www.youtube.com/embed/" + id + "?enablejsapi=1&origin=http://example.com";
+//   //   var iframe  = document.createElement("iframe");
+//   //       iframe.className = "video";
+//   //       iframe.setAttribute("src", videoURL);
+//   //       console.log(iframe)
+//   //       recipeModalContentEl.appendChild(iframe);
+//    }
 
-var videoBtnEl = document.getElementById("video-btn");
-videoBtnEl.addEventListener("click", function () {
-  getYT();
-});
+
+// function displayYT(id) {
+//   create DOM element to append to modal
+//   var playerEl = document.getElementById("player");
+//   playerEl.setAttribute(
+//     "src",
+//     "http://www.youtube.com/embed/" +
+//       id +
+//       "?enablejsapi=1&origin=http://example.com"
+//   );
+// }
+
+//var videoBtnEl = document.getElementById("video-btn");
+//videoBtnEl.addEventListener("click", function () {
+  //getYT();
+//});
 
 // when a recipe is searched its put in local storage
 analyzeRecipeEl.addEventListener("click", function() {
     searchTerm = document.getElementById("name-input").value;
     getRecipe(searchTerm);
-    if (searchHistory.includes(searchTerm) == false) {
-        searchHistory.push(searchTerm);
-    }
-    localStorage.setItem("search",JSON.stringify(searchHistory));
+    // if (searchHistory.includes(searchTerm) == false) {
+    //     searchHistory.push(searchTerm);
+    // }
+    // localStorage.setItem("search",JSON.stringify(searchHistory));
    //displaySearchHistory();
 })
 // when the clear history button is pressed it clears storage
-/*clearHistoryEl.addEventListener("click",function() {
-    searchHistory = [];
-    localStorage.setItem("search",JSON.stringify(searchHistory));
-    displaySearchHistory();
- })*/
+// /*clearHistoryEl.addEventListener("click",function() {
+//     searchHistory = [];
+//     localStorage.setItem("search",JSON.stringify(searchHistory));
+//     displaySearchHistory();
+//  })*/
 
  //will display past searches on side
-/*function displaySearchHistory() {
-   historyEl.innerHTML = "";
-   for (var i = 0; i < searchHistory.length; i++) {
-      var pastRecipe = document.createElement("input");
-      pastRecipe.setAttribute("type","text");
-      pastRecipe.setAttribute("readonly",true);
-      pastRecipe.setAttribute("class", "recipe-space");
-      pastRecipe.setAttribute("value", searchHistory[i]);
-      let recipeNames = searchHistory[i];
-      pastRecipe.addEventListener("click",function() {
-          console.log(this.value);
-          console.log(recipeNames);
-          var searchInput =  document.getElementById("name-input");
-          searchInput.value = recipeNames;
-         getRecipe(recipeNames);  
-      })
-      historyEl.append(pastRecipe);
+//function displaySearchHistory() {
+//    historyEl.innerHTML = "";
+//    for (var i = 0; i < searchHistory.length; i++) {
+//       var pastRecipe = document.createElement("input");
+//       pastRecipe.setAttribute("type","text");
+//       pastRecipe.setAttribute("readonly",true);
+//       pastRecipe.setAttribute("class", "recipe-space");
+//       pastRecipe.setAttribute("value", searchHistory[i]);
+//       let recipeNames = searchHistory[i];
+//       pastRecipe.addEventListener("click",function() {
+//           console.log(this.value);
+//           console.log(recipeNames);
+//           var searchInput =  document.getElementById("name-input");
+//           searchInput.value = recipeNames;
+//          getRecipe(recipeNames);  
+//       })
+//       historyEl.append(pastRecipe);
       
 
-}}
+// }}
 
-displaySearchHistory();
-if (searchHistory.length > 0) {
-   getRecipe(searchHistory[searchHistory.length - 1]);
-}*/
-
+// displaySearchHistory();
+// if (searchHistory.length > 0) {
+//    getRecipe(searchHistory[searchHistory.length - 1]);}
