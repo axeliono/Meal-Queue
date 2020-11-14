64965bf24ac0d8091a7262b778d5603729ba34db
j9210 <71297542+j9210@users.noreply.github.com>
2020-11-13 19:01:34 -0600

    pulling

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,6 +112,7 @@ function displayRecipeCards(recipeName, recipeImage, ingredientArrayObject) {
     }
   });
   btnLabel.addEventListener("click", populateModalContent);
+  btnLabel.addEventListener("click", getYT());
 
   //modal content labels and elements
   recipeModalContentEl = document.createElement("div");
